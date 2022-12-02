1201744fed933981bc9ce58b9825c3f82f64c8da
takleung <takleung@gmail.com>
2022-12-02 21:32:09 +0800

    Update 00-startup.sh

--- RBAC/00-startup.sh
+++ RBAC/00-startup.sh
@@ -1,4 +1,4 @@
-oc login -u admin -p nopass https://api.poc1.rhocp.hkjc:6443
+oc login -u hkex-admin -p nopass https://api.hkexpoc.redhathk.com:6443
 
 oc get clusterrolebinding -o wide \
     | grep -E 'NAME|self-provisioner'
@@ -13,27 +13,27 @@ oc describe clusterrolebindings self-provisioners
 oc get clusterrolebinding -o wide \
     | grep -E 'NAME|self-provisioner'
 
-oc login -u leader -p nopass
+oc login -u hkex-leader -p nopass
 
 oc new-project test
 
-oc login -u admin -p nopass
+oc login -u hkex-admin -p nopass
 
 oc new-project authorization-rbac
 
-oc policy add-role-to-user admin leader -n authorization-rbac
+oc policy add-role-to-user admin hkex-leader -n authorization-rbac
 
 oc adm groups new dev-group
 
-oc adm groups add-users dev-group developer
+oc adm groups add-users dev-group hkex-developer
 
 oc adm groups new qa-group
 
-oc adm groups add-users qa-group qa-engineer
+oc adm groups add-users qa-group hkex-qa-engineer
 
 oc get groups
 
-oc login -u leader -p nopass
+oc login -u hkex-leader -p nopass
 
 oc policy add-role-to-group edit dev-group -n authorization-rbac
 
@@ -41,17 +41,17 @@ oc policy add-role-to-group view qa-group -n authorization-rbac
 
 oc get rolebindings -o wide
 
-oc login -u developer -p developer
+oc login -u hkex-developer -p developer
 
 oc new-app --name httpd httpd:2.4 -n authorization-rbac
 
-oc policy add-role-to-user edit qa-engineer
+oc policy add-role-to-user edit hkex-qa-engineer
 
 oc login -u qa-engineer -p nopass
 
 oc scale deployment httpd --replicas 3
 
-oc login -u admin -p nopass
+oc login -u hkex-admin -p nopass
 
 oc adm policy add-cluster-role-to-group \
   --rolebinding-name self-provisioners \
